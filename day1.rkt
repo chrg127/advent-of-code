68510ad9f351ecc9e1c2a3bd1c47e4e812b21919
chrg127 <christian.ricci2000@gmail.com>
2022-12-01 16:31:57 +0100

    Day 1

---
+++ day1.rkt
@@ -0,0 +1,17 @@
+(define (part1 input)
+  (println (apply max (map (lambda (x) (apply + x))
+                           (map (lambda (x) (map string->number (string-split x "\n")))
+                                (string-split input "\n\n"))))))
+
+(define (part2 input)
+  (println (apply + (take (sort
+      (map (lambda (x) (apply + x))
+                    (map (lambda (x) (map string->number (string-split x "\n")))
+                         (string-split input "\n\n"))) >) 3))))
+
+(define input1 (file->string "input1-1.txt"))
+(define input2 (file->string "input1-2.txt"))
+(part1 input1)
+(part1 input2)
+(part2 input1)
+(part2 input2)
