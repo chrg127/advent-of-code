ad5e0a7395c769210644b729f1a5f3a8830a481b
chrg <christian.ricci2000@gmail.com>
2022-12-03 14:15:48 +0100

    Day 3 part 2 (in progress)

--- day3.rkt
+++ day3.rkt
@@ -3,6 +3,8 @@
     (- (char->integer item) 38)
     (- (char->integer item) 96)))
 
+(define 
+
 (define (find-item rucksack)
   (let* ((half (/ (string-length rucksack) 2))
          (comp1 (list->set (string->list (substring rucksack 0 half))))
@@ -12,7 +14,19 @@
 (define (part1 input)
   (println (apply + (map (lambda (r) (priority-of (find-item r))) input))))
 
+(define (split-by lst n)
+  (if (not (empty? lst))
+    (cons (take lst n) (split-by (drop lst n) n))
+    '()))
+
+(define (find-badge groups)
+  )
+
+(define (part2 input)
+  (println (split-by input 3)))
+
 (define input1 (file->lines "input3-1.txt"))
 (define input2 (file->lines "input3-2.txt"))
-(part1 input1)
-(part1 input2)
+; (part1 input1)
+; (part1 input2)
+(part2 input1)
