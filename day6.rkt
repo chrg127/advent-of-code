d2d84ed24ca01d7b2721e1084c6cac43d81ab8c8
chrg127 <christian.ricci2000@gmail.com>
2022-12-06 09:29:40 +0100

    Day 6

---
+++ day6.rkt
@@ -0,0 +1,15 @@
+(define (slide s n)
+  (for/first ([i (in-range (- (string-length s) n))]
+              #:when (not (check-duplicates
+                            (string->list (substring s i (+ i n))))))
+      (+ n i)))
+
+(define (part1 input) (for-each (lambda (x) (println (slide x 4))) input))
+(define (part2 input) (for-each (lambda (x) (println (slide x 14))) input))
+
+(time (begin (define input1 (file->lines "input6-1.txt"))
+             (define input2 (file->lines "input6-2.txt"))
+             (part1 input1)
+             (part1 input2)
+             (part2 input1)
+             (part2 input2)))
