fcd5959f0181cad0253a4ad90f573decba2a2acf
chrg <christian.ricci2000@gmail.com>
2022-12-06 19:43:13 +0100

    Make day 6 even shorter

--- day6.rkt
+++ day6.rkt
@@ -1,11 +1,11 @@
-(define (slide s n)
+(define ((slide n) s)
   (for/first ([i (in-range (- (string-length s) n))]
               #:when (not (check-duplicates
                             (string->list (substring s i (+ i n))))))
       (+ n i)))
 
-(define (part1 input) (for-each (lambda (x) (println (slide x 4))) input))
-(define (part2 input) (for-each (lambda (x) (println (slide x 14))) input))
+(define (part1 input) (println (map (slide 4) input)))
+(define (part2 input) (println (map (slide 14) input)))
 
 (time (begin (define input1 (file->lines "input6-1.txt"))
              (define input2 (file->lines "input6-2.txt"))
