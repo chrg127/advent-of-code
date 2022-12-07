1b21bf8326f840d4deab6c54cfc01c59e920bc31
chrg <christian.ricci2000@gmail.com>
2022-12-07 21:39:35 +0100

    Slight update to day 7

--- day7.rkt
+++ day7.rkt
@@ -1,4 +1,4 @@
-(require srfi/1)
+(require srfi/1) ; list-index
 
 (define (update-dir cwd i new-dir)
   (list-update cwd 2 (lambda (c) (list-set c i new-dir))))
@@ -6,17 +6,11 @@
 (define (chdir dir lines cwd)
   (if (string=? dir "..")
     (list 'early-break cwd)
-    (let ([i (list-index (lambda (c) (and (eq? (car c) 'dir)
+    (let* ([i (list-index (lambda (c) (and (eq? (car c) 'dir)
                                           (string=? (cadr c) dir)))
-                         (third cwd))])
-      (let ([r (step lines (list-ref (third cwd) i))])
-        (list (car r) (update-dir cwd i (cadr r)))))))
-
-(define (parse-cmd line lines cwd)
-  (let ([splitted (drop (string-split line " ") 1)])
-    (if (not (string=? (car splitted) "cd"))
-      (list lines cwd) ; ls commands are ignored
-      (chdir (cadr splitted) lines cwd))))
+                         (third cwd))]
+           [r (step lines (list-ref (third cwd) i))])
+      (list (car r) (update-dir cwd i (cadr r))))))
 
 (define (new-file line)
   (let ([splitted (string-split line " ")])
@@ -28,17 +22,18 @@
   (memf (lambda (f) (and (eq? (car f) (car file))
                          (string=? (cadr f) (cadr file)))) files))
 
-(define (add-child dir child)
-  (list-update dir 2 (lambda (x) (if (dup-file? x child)
-                                   x
-                                   (append x (list child))))))
+(define (add-file dir file)
+  (list-update dir 2 (lambda (c)
+                       (if (dup-file? c file) c (cons file c)))))
 
 (define (parse-line line lines cwd)
-  (if (not (char=? (string-ref line 0) #\$))
-    (list lines (add-child cwd (new-file line)))
-    (parse-cmd line lines cwd)))
+  (cond ((not (char=? (string-ref line 0) #\$))
+         (list lines (add-file cwd (new-file line))))
+        ((string=? line "$ ls")
+         (list lines cwd)) ; ls commands are ignored
+        (else (chdir (substring line 5) lines cwd))))
 
-(define (step lines cwd)
+(define (step lines [cwd (list 'dir "/" '())])
   (if (empty? lines)
     (list '() cwd)
     (let ([r (parse-line (car lines) (cdr lines) cwd)])
@@ -46,8 +41,7 @@
         (list (cdr lines) (cadr r))
         (apply step r)))))
 
-(define (make-tree input)
-  (cadr (step (cdr input) (list 'dir "/" '()))))
+(define (make-tree input) (cadr (step (cdr input))))
 
 (define (total-size dir)
   (if (eq? (car dir) 'file)
@@ -75,7 +69,9 @@
     (println
       (apply min (filter-sizes (lambda (s) (> (+ unused s) 30000000))
                                (make-tree input))))))
-(define (pretty-printer tree level)
+
+; not actually part of the solution, but it's neat
+(define (pretty-printer tree [level 0])
   (printf "~a- ~a ~a\n"
           (make-string (* level 2) #\ )
           (cadr tree)
