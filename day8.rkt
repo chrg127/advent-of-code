0cc79b8399a3cf67349d119e981cde2b3b2454b2
chrg <christian.ricci2000@gmail.com>
2022-12-08 22:18:27 +0100

    Day 8 part 2

--- day8.rkt
+++ day8.rkt
@@ -1,12 +1,28 @@
 (require srfi/1) ; for zip
 
-(define (find-visibles lst [max-tree -1] [visibles '()])
-  (if (empty? lst)
-    visibles
-    (let* ([tree-value (caar lst)])
-      (if (> tree-value max-tree)
-        (find-visibles (cdr lst) tree-value (cons (cadr (car lst)) visibles))
-        (find-visibles (cdr lst) max-tree visibles)))))
+(define ((loop-row init collect) row [len (length row)] [pos 0])
+  (if (empty? (cdr row))
+    (list (init row pos))
+    (let* ([res ((loop-row init collect) (cdr row) len (add1 pos))])
+      (collect row len pos res))))
+
+(define score-row
+  (loop-row (lambda (row pos) (list (caar row) pos 0))
+            (lambda (row len pos res)
+              (let* ([higher (filter (lambda (x) (>= (car x) (caar row))) res)]
+                     [min-pos (if (empty? higher)
+                                (sub1 len)
+                                (cadr (argmin cadr higher)))])
+                (cons (list (caar row) pos (- min-pos pos)) res)))))
+
+(define (find-visibles row)
+  (define the-loop
+    (loop-row (lambda (row pos) (car row))
+              (lambda (row len pos res)
+                (if (> (caar row) (apply max (map car res)))
+                  (cons (car row) res)
+                  res))))
+  (map cadr (the-loop row)))
 
 (define (make-grid input)
   (for/list ([i (in-range (length input))])
@@ -18,17 +34,28 @@
   (for/list ([col (in-range (length grid))])
     (map (lambda (v) (list-ref v col)) grid)))
 
-(define (find-all-visibles grid)
-  (let ([t (traspose grid)])
-    (append (apply append (map find-visibles grid))
-            (apply append (map (compose find-visibles reverse) grid))
-            (apply append (map find-visibles t))
-            (apply append (map (compose find-visibles reverse) t)))))
+(define (find-all-visibles grid tr)
+  (append (apply append (map find-visibles grid))
+          (apply append (map (compose find-visibles reverse) grid))
+          (apply append (map find-visibles tr))
+          (apply append (map (compose find-visibles reverse) tr))))
+
+(define (scenic-score-all grid tr)
+  (map *
+       (flatten           (map (lambda (r) (map caddr          (score-row          r)))   grid))
+       (flatten           (map (lambda (r) (map caddr (reverse (score-row (reverse r))))) grid))
+       (flatten (traspose (map (lambda (r) (map caddr          (score-row          r)))   tr)))
+       (flatten (traspose (map (lambda (r) (map caddr (reverse (score-row (reverse r))))) tr)))))
 
 (define (part1 grid)
-  (println (length (remove-duplicates (find-all-visibles grid)))))
+  (println (length (remove-duplicates (find-all-visibles grid (traspose grid))))))
 
-(define input1 (make-grid (file->lines "input8-1.txt")))
-(define input2 (make-grid (file->lines "input8-2.txt")))
-(part1 input1)
-(part1 input2)
+(define (part2 grid)
+  (println (apply max (scenic-score-all grid (traspose grid)))))
+
+(time (begin (define input1 (make-grid (file->lines "input8-1.txt")))
+             (define input2 (make-grid (file->lines "input8-2.txt")))
+             (part1 input1)
+             (part1 input2)
+             (part2 input1)
+             (part2 input2)))
