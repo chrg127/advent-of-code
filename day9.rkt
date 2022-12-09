12717839203b5864494648f4c95d917377391603
chrg <christian.ricci2000@gmail.com>
2022-12-10 00:18:33 +0100

    Update day 9

--- day9.rkt
+++ day9.rkt
@@ -4,12 +4,11 @@
 
 (define (array+ . args) (apply (curry map +) args))
 (define (array- . args) (apply (curry map -) args))
+(define (touching? h t) (andmap (lambda (x) (<= x 1)) (map abs (array- h t))))
+(define ((clamp a b) x) (min b (max a x)))
+(define (move-tail t h) (array+ t (map (clamp -1 1) (array- h t))))
 (define (make-move instr)
-  (hash-ref (hash #\R '( 1  0) #\L '(-1  0) #\U '( 0 -1) #\D '( 0  1)) instr))
-(define (touching? h t)
-  (andmap (lambda (x) (<= x 1)) (map abs (array- h t))))
-(define (normalize x) (cond ((> x 1) 1) ((< x -1) -1) (else x)))
-(define (move-tail t h) (array+ t (map normalize (array- h t))))
+  (hash-ref (hash #\R '(1 0) #\L '(-1 0) #\U '(0 -1) #\D '(0 1)) instr))
 
 (define (move-rope rope move)
   (if (empty? (cdr rope)) ; head?
@@ -19,14 +18,13 @@
       (cons (if (touching? h t) t (move-tail t h)) rest))))
 
 (define (move-in-steps data move steps)
-  (foldl (lambda (x r)
-           (let ([rope (move-rope (car r) (make-move move))])
-             (list rope (set-add (cadr r) (car rope)))))
+  (foldl (lambda (x r) (let ([rope (move-rope (car r) move)])
+                         (list rope (set-add (cadr r) (car rope)))))
          data
          (range steps)))
 
 (define (apply-instrs instrs rope-length)
-  (foldl (lambda (x r) (move-in-steps r (car x) (cadr x)))
+  (foldl (lambda (x r) (move-in-steps r (make-move (car x)) (cadr x)))
          (list (build-list rope-length (lambda (x) '(0 4))) (set))
          instrs))
 
@@ -40,6 +38,7 @@
              (define input3 (parse (file->lines "input9-3.txt")))
              (part1 input1)
              (part1 input2)
+             (part1 input3)
              (part2 input1)
              (part2 input3)
              (part2 input2)))
