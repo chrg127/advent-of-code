4ecc5fbb2dc9ce4c180838ba61306c82c7d11b84
chrg <christian.ricci2000@gmail.com>
2022-12-17 21:08:43 +0100

    Day 16 (WIP)

--- day16.rkt
+++ day16.rkt
@@ -1,115 +1,98 @@
-(define (zip . args) (apply map list args))
-(define (make-indexes graph)
-  (apply hash (flatten (zip (map car graph) (range (length graph))))))
-(define (apply-indexes graph indexes)
-  (map (lambda (n) (append (take n 2) (map (curry hash-ref indexes) (cddr n)))) graph))
+(require graph)
+
+(define (list-index f l [i 0])
+  (cond ((empty? l) #f)
+        ((f (car l)) i)
+        (else (list-index f (cdr l) (+ 1 i)))))
+
+(define (find-node g name) (list-index (compose (curry string=? name) car) g))
+(define (node-name g i) (car (list-ref g i)))
+(define (node-rate g i) (cadr (list-ref g i)))
+
+(define (make-graph input)
+  (define (make-edge-pairs node data)
+    (map (compose (curry list node) (curry find-node input)) (cddr data)))
+  (unweighted-graph/directed
+    (apply append (map make-edge-pairs (range (length input)) input))))
+
 (define (parse input)
-  (let ([res (map (lambda (l) (list-update (regexp-match* "[A-Z][A-Z]|[0-9]+" l)
-                                           1 string->number)) input)])
-    (apply-indexes res (make-indexes res))))
-
-(define (find-node g name)
-  (index-of g (findf (compose (curry string=? name) car) g)))
-(define (node-name graph i) (car (list-ref graph i)))
-(define (node-rate graph i) (cadr (list-ref graph i)))
-
-(define (node-list g v)
-  (make-hash (map (lambda (i) (cons i v)) (range (length g)))))
-(define (extract-min q d)
-  (foldl (lambda (x y) (if (< (hash-ref d x) (hash-ref d y)) x y))
-         (car q) (cdr q)))
-
-(define (get-adj g u visited)
-  (filter (lambda (x) (not (set-member? visited x))) (cddr (list-ref g u))))
-
-(define (dijkstra graph start)
-  (let* ([dist (node-list graph (expt 2 64))]
-         [prev (node-list graph '())]
-         [q (range (length graph))]
-         [visited (mutable-set)])
-    (hash-set! dist start 0)
-    (do () ((empty? q))
-      (let* ([u (extract-min q dist)])
-        (set! q (remove u q))
-        (set-add! visited u)
-        (for-each (lambda (v)
-                    (let ([tmp (+ 1 (hash-ref dist u))])
-                      (when (< tmp (hash-ref dist v))
-                        (begin (hash-set! dist v tmp)
-                               (hash-set! prev v u)))))
-                  (get-adj graph u visited))))
-    (values dist prev)))
+  (let ([data (map (lambda (l) (list-update (regexp-match* "[A-Z][A-Z]|[0-9]+" l)
+                                            1 string->number)) input)])
+    (values (make-graph data) data)))
 
 (define (ratio g ds n)
   (let ([d (hash-ref ds n)])
     (if (zero? d) -1 (/ (cadr (list-ref g n)) d))))
 
-(define (sort-valves graph dists)
-  (sort (range (length graph))
-        (lambda (x y) (> (ratio graph dists x) (ratio graph dists y)))))
+(define (sort-valves g ds)
+  (sort (range (length g)) (lambda (x y) (> (ratio g ds x) (ratio g ds y)))))
 
-(define (dist-between graph x y)
-  (let-values ([(dist prev) (dijkstra graph x)])
-    (hash-ref dist y)))
+; (define (dist-between graph x y)
+;   (let-values ([(dist prev) (dijkstra graph x)])
+;     (hash-ref dist y)))
 
-(define (dist-loop g last cur valves [minutes 30] [pressure 0])
-  (let* ([d (dist-between g last cur)]
-         [new-min (- minutes (+ d 1))]
-         [new-pressure (+ pressure (* (node-rate g cur) new-min))])
-    (if (or (empty? valves) (<= new-min 0))
-      pressure
-      (begin
-        (printf "between ~a, ~a: ~a; r = ~a, m = ~a, p = ~a\n"
-                (node-name g last)
-                (node-name g cur)
-                d
-                (node-rate g cur)
-                new-min
-                new-pressure)
-        (dist-loop g cur (car valves) (cdr valves) new-min new-pressure)))))
+; (define (dist-loop g last cur valves [minutes 30] [pressure 0])
+;   (let* ([d (dist-between g last cur)]
+;          [new-min (- minutes (+ d 1))]
+;          [new-pressure (+ pressure (* (node-rate g cur) new-min))])
+;     (if (or (empty? valves) (<= new-min 0))
+;       pressure
+;       (begin
+;         (printf "between ~a, ~a: ~a; r = ~a, m = ~a, p = ~a\n"
+;                 (node-name g last) (node-name g cur) d (node-rate g cur) new-min new-pressure)
+;         (dist-loop g cur (car valves) (cdr valves) new-min new-pressure)))))
 
-(define (part1 graph)
-  (let-values ([(dist prev) (dijkstra graph (find-node graph "AA"))])
-    (let ([sorted (sort-valves graph dist)])
-      (for-each (lambda (x) (printf "(~a ~a)\n" (node-name graph x) (hash-ref dist x))) sorted)
-      (println (map (curry node-name graph) sorted))
-      (println (dist-loop graph (find-node graph "AA") (car sorted) (cdr sorted))))))
+(define (memo-walk graph data dists)
+  (define memo (make-hash))
+  (define (loop cur opened minutes)
+    (let ([memo-res (hash-ref memo (list cur opened minutes))])
+      (if memo-res
+        memo-res
+        (let* ([possible (find-possible-next-nodes opened)]
+               [the-result (apply max (map (lambda (n) (loop n opened minutes))))])
+          (hash-set! memo (list cur opened minutes) the-result)
+          the-result)))))
 
-(define (get-next-valve g cur opened)
-  (let-values ([(dist prev) (dijkstra g cur)])
-    (let* ([sorted (sort-valves g dist)]
-           [possible (filter (lambda (x) (not (member x opened))) sorted)])
-      (values (car possible) (hash-ref dist (car possible))))))
+(define (part1 graph data)
+  (let-values ([(dist prev) (dijkstra graph (find-node data "AA"))])
+    (let ([sorted (sort-valves data dist)])
+      (for-each (lambda (x) (printf "(~a ~a ~a)\n"
+                                    (node-name data x)
+                                    (hash-ref dist x)
+                                    (exact->inexact (ratio data dist x)))) sorted))))
+      ; (println (dist-loop graph (find-node graph "AA") (car sorted) (cdr sorted))))))
 
-(define (dist-loop g last cur valves [minutes 30] [pressure 0])
-  (let* ([d (dist-between g last cur)]
-         [new-min (- minutes (+ d 1))]
-         [new-pressure (+ pressure (* (node-rate g cur) new-min))])
-    (if (or (empty? valves) (<= new-min 0))
-      pressure
-      (begin
-        (printf "between ~a, ~a: ~a; r = ~a, m = ~a, p = ~a\n"
-                (node-name g last) (node-name g cur) d (node-rate g cur) new-min new-pressure)
-        (dist-loop g cur (car valves) (cdr valves) new-min new-pressure)))))
+; (define (get-next-valve g cur opened)
+;   (let-values ([(dist prev) (dijkstra g cur)])
+;     (let* ([sorted (sort-valves g dist)]
+;            [possible (filter (lambda (x) (not (member x opened))) sorted)])
+;       (values (car possible) (hash-ref dist (car possible))))))
 
-(define (dist-loop-2 g cur [opened '()] [minutes 30] [pressure 0])
-  (let-values ([(next d) (get-next-valve g cur opened)])
-    (let* ([new-min (- minutes (+ d 1))]
-           [new-pressure (+ pressure (* (node-rate g next) new-min))])
-      (if (<= new-min 0)
-        pressure
-        (begin
-          (printf "between ~a, ~a: ~a; r = ~a, m = ~a, p = ~a\n"
-                  (node-name g cur) (node-name g next) d (node-rate g next) new-min new-pressure)
-          (dist-loop-2 g next (cons next opened) new-min new-pressure))))))
+; (define (dist-loop-2 g cur [opened '()] [minutes 30] [pressure 0])
+;   (let-values ([(next d) (get-next-valve g cur opened)])
+;     (let* ([new-min (- minutes (+ d 1))]
+;            [new-pressure (+ pressure (* (node-rate g next) new-min))])
+;       (if (<= new-min 0)
+;         pressure
+;         (begin
+;           (printf "between ~a, ~a: ~a; r = ~a, m = ~a, p = ~a\n"
+;                   (node-name g cur) (node-name g next) d (node-rate g next) new-min new-pressure)
+;           (dist-loop-2 g next (cons next opened) new-min new-pressure))))))
 
-(define (part1-2 graph)
-  (for-each println (sort graph (lambda (x y) (> (cadr x) (cadr y)))))
-  (println (dist-loop-2 graph (find-node graph "AA"))))
+; (define (part1-2 graph)
+;   (for-each println (sort graph (lambda (x y) (> (cadr x) (cadr y)))))
+;   (println (dist-loop-2 graph (find-node graph "AA"))))
 
-(define input1 (parse (file->lines "input16-1.txt")))
-(define input2 (parse (file->lines "input16-2.txt")))
-(part1 input1)
-(part1-2 input1)
-(part1 input2)
-(part1-2 input2)
+; (define input1 (parse (file->lines "input16-1.txt")))
+
+(define-values (graph1 data1) (parse (file->lines "input16-1.txt")))
+(part1 graph1 data1)
+(println (floyd-warshall graph1))
+
+; (define input2 (parse (file->lines "input16-2.txt")))
+; (part1 input1)
+; (part1 input2)
+; (part1 input3)
+; (part1-2 input2)
+; (part1-2 input1)
+; (part1-2 input3)
