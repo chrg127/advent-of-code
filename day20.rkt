608af0c2a760ba4b3dc9706f51bc0de99d071c78
chrg <christian.ricci2000@gmail.com>
2022-12-20 22:54:18 +0100

    Updated day 20

--- day20.rkt
+++ day20.rkt
@@ -5,68 +5,51 @@
         ((f (car l)) i)
         (else (list-index f (cdr l) (+ 1 i)))))
 
-(define (remove-index i lst)
-  (cond ((empty? lst) '())
-        ((= i 0) (cdr lst))
-        (else (cons (car lst) (remove-index (- i 1) (cdr lst))))))
+(define (remove-index i l)
+  (cond ((empty? l) '())
+        ((= i 0) (cdr l))
+        (else (cons (car l) (remove-index (- i 1) (cdr l))))))
 
-(define (add-index v i lst)
-  (cond ((= i 0) (cons v lst))
-        ((empty? lst) '())
-        (else (cons (car lst) (add-index v (- i 1) (cdr lst))))))
+(define (add-after v n l)
+  (cond ((= n 0) (cons v l))
+        ((empty? l) '())
+        (else (cons (car l) (add-after v (- n 1) (cdr l))))))
 
 (define (add-between v i j lst)
-  (if (> i j) ; we are on the border (this is seriously the solution?)
-    (add-index v (+ i 1) lst)
-    (add-index v j lst)))
+  (add-after v (if (> i j) (+ i 1) j) lst))
 
 (define (move n from to-1 to-2 nums)
-  (if (= from to-2)
-    nums
-    (add-between n to-1 to-2 (remove-index from nums))))
+  (if (= from to-2) nums (add-between n to-1 to-2 (remove-index from nums))))
 
 (define (get-pos n i len)
-  (list (modulo (+ n i -1) (- len 1))
-        (modulo (+ n i) (- len 1))))
+  (list (modulo (+ n i -1) (- len 1)) (modulo (+ n i) (- len 1))))
 
 (define (move-num nums indexes i len)
   (let* ([from (list-index (lambda (x) (= x i)) indexes)]
          [n (list-ref nums from)]
-         [to (get-pos n from len)]
-         [res (move n from (car to) (cadr to) nums)]
-         [is  (move i from (car to) (cadr to) indexes)])
-    ;(printf "i = ~a, move ~a from ~a to ~a, res = ~a, is = ~a\n" i n from to res is)
-    (list res is)))
+         [to (get-pos n from len)])
+    (list (move n from (car to) (cadr to) nums)
+          (move i from (car to) (cadr to) indexes))))
 
-(define (step-n input [indexes (range (length input))]
-                [steps (length input)] [len (length input)])
-  (foldl (lambda (i r)
-           (let ([nums (car r)] [indexes (cadr r)])
-             (move-num nums indexes i len)))
+(define (mix input [indexes (range (length input))])
+  (foldl (lambda (i r) (move-num (car r) (cadr r) i (length input)))
          (list input indexes)
-         (range steps)))
+         (range (length input))))
 
-(define (get-nth n nums)
-  (let ([zero-index (list-index zero? nums)])
-    (list-ref nums (modulo (+ n zero-index) (length nums)))))
-
-(define (find-grove nums)
-  (+ (get-nth 1000 nums) (get-nth 2000 nums) (get-nth 3000 nums)))
-
-(define (part1 input)
-  (println (find-grove (car (step-n input)))))
+(define ((get-nth nums) n)
+  (list-ref nums (modulo (+ n (list-index zero? nums)) (length nums))))
+(define (find-grove nums) (apply + (map (get-nth nums) '(1000 2000 3000))))
 
 (define (rounds nums num-rounds)
-  (foldl (lambda (i ns)
-           ; (printf "round ~a: ~a\n" i ns)
-           (step-n (car ns) (cadr ns)))
+  (foldl (lambda (i ns) (mix (car ns) (cadr ns)))
          (list nums (range (length nums)))
          (range num-rounds)))
 
-(define (part2 input)
-  (let ([decrypted (map (lambda (x) (* x 811589153)) input)])
-    (println (find-grove (car (rounds decrypted 10))))))
+(define (part1 input) (println (find-grove (car (mix input)))))
 
+(define (part2 input)
+  (println (find-grove (car (rounds (map (lambda (x) (* x 811589153))
+                                         input) 10)))))
 (define input1 (ints (file->string "input20-1.txt")))
 (define input2 (ints (file->string "input20-2.txt")))
 (part1 input1)
