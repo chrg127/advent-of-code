d16c7f7ca194aa70fd3141e9ed95ce4ed5967492
chrg <christian.ricci2000@gmail.com>
2022-12-25 22:40:11 +0100

    Update day 25

--- day25.rkt
+++ day25.rkt
@@ -1,6 +1,5 @@
 (define (snafu->digit c) (hash-ref (hash #\= -2 #\- -1 #\0 0 #\1 1 #\2 2) c))
 (define (digit->snafu c) (hash-ref (hash -2 #\= -1 #\- 0 #\0 1 #\1 2 #\2) c))
-
 (define (snafu->string n) (list->string (map digit->snafu n)))
 
 (define (snafu->number s)
@@ -10,12 +9,10 @@
 
 (define (number->snafu n)
   (define (loop n)
-    (if (= n 0)
-      '()
-      (let-values ([(div mod) (quotient/remainder n 5)])
-        (if (> mod 2)
-          (cons (- mod 5) (loop (+ 1 div)))
-          (cons mod (loop div))))))
+    (let-values ([(div mod) (quotient/remainder n 5)])
+      (cond ((= n 0) '())
+            ((> mod 2) (cons (- mod 5) (loop (+ 1 div))))
+            (else (cons mod (loop div))))))
   (reverse (loop n)))
 
 (define (part1 input)
