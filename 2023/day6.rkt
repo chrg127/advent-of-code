dbbde1072efb0013c694e44f8bc808230b439f40
chrg <christian.ricci2000@gmail.com>
2023-12-06 19:14:59 +0100

    Day 6 clean up

--- 2023/day6.rkt
+++ 2023/day6.rkt
@@ -6,6 +6,10 @@
   (let ([sols (solve-2order-eq -1 time (- best-dist))])
     (sub1 (ceiling (- (ceiling (cadr sols)) (car sols))))))
 
+;; older find-options, kept here for history
+(define (old-find-options time dist)
+  (length (filter (lambda (x) (> (* x (- time x)) dist)) (range (+ time 1)))))
+
 (define (parse process in)
   (map (lambda (x) (map string->number (process (regexp-match* #rx"[0-9]+" x))))
        (file->lines in)))
@@ -13,17 +17,9 @@
 (define (join-nums nums) (list (apply string-append nums)))
 
 (define (solve process in)
-  (let ([parsed (parse process in)])
-    (apply * (map (lambda (time dist) (find-options time dist))
-                  (car parsed) (cadr parsed)))))
+  (apply * (apply (curry map find-options) (parse process in))))
 
 (println (solve identity "input6-1.txt"))
 (println (solve identity "input6-2.txt"))
 (println (solve join-nums "input6-1.txt"))
 (println (solve join-nums "input6-2.txt"))
-
-;; older find-options, kept here for history
-(define (old-find-options time best-dist)
-  (length (filter (lambda (x) (> x best-dist))
-                  (map (lambda (h) (* h (- time h)))
-                       (range (+ time 1))))))
