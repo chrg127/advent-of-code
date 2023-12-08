03ec3f022246d976254362b3bd89ddf73d8aa7fe
chrg <christian.ricci2000@gmail.com>
2023-12-08 23:15:57 +0100

    Day 8 (cleaned up)

--- 2023/day8.rkt
+++ 2023/day8.rkt
@@ -1,16 +1,14 @@
+(require srfi/1)
+
 (define (end=? c s) (char=? (string-ref s (- (string-length s) 1)) c))
-
-(define (build-node-path instrs paths [pos "AAA"] [i 0])
-  (let* ([n (modulo i (length instrs))]
-         [next ((list-ref instrs n) (hash-ref paths pos))])
-    (stream-cons (list pos n) (build-node-path instrs paths next (+ i 1)))))
-
-(define (find-zzz pred instrs paths [pos "AAA"])
-  (for/or ([pos (build-node-path instrs paths pos)] [i (in-naturals)])
-    (if (pred (car pos)) i #f)))
-
 (define pred1 (lambda (x) (string=? x "ZZZ")))
 (define pred2 (lambda (x) (end=? #\Z x)))
+
+(define (path-loop pred instrs paths [pos "AAA"] [i 0])
+  (if (pred pos)
+    i
+    (let* ([next ((car instrs) (hash-ref paths pos))])
+      (path-loop pred (cdr instrs) paths next (+ i 1)))))
 
 (define (parse in)
   (let* ([split (string-split (file->string in) "\n\n")]
@@ -20,13 +18,13 @@
                        `(,(substring line 0 3)
                           (,(substring line 7 10) ,(substring line 12 15))))
                      (string-split (cadr split) "\n"))])
-    (list instrs (apply hash (apply append paths)))))
+    (list (apply circular-list instrs) (apply hash (apply append paths)))))
 
-(define (solve1 in) (apply (curry find-zzz pred1) (parse in)))
+(define (solve1 in) (apply (curry path-loop pred1) (parse in)))
 
 (define (solve2 in)
   (let ([ins (parse in)])
-    (apply lcm (map (lambda (x) (find-zzz pred2 (car ins) (cadr ins) x))
+    (apply lcm (map (lambda (x) (path-loop pred2 (car ins) (cadr ins) x))
                     (filter (curry end=? #\A) (hash-keys (cadr ins)))))))
 
 (println (solve1 "input8-1.txt"))
