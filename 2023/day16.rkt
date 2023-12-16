9d93d9a385b6215ecade7c2692a24976c9e56c0f
chrg <christian.ricci2000@gmail.com>
2023-12-16 15:49:52 +0100

    Day 16

--- 2023/day16.rkt
+++ 2023/day16.rkt
@@ -1,29 +1,21 @@
 (define (parse in)
   (list->vector (map (compose list->vector string->list) (file->lines in))))
 
+(define (grid-ref g p) (vector-ref (vector-ref g (cadr p)) (car p)))
+(define (swap-xy p) (list (cadr p) (car p)))
 (define (in-bounds? g p)
   (andmap (lambda (x) (and (>= x 0) (< x (vector-length g)))) p))
 
-(define (grid-ref g p) (vector-ref (vector-ref g (cadr p)) (car p)))
-
-(define vec+ (curry map +))
-(define (swap-xy p) (list (cadr p) (car p)))
-
 (define (get-new-dir symb dir)
-  (cond ([char=? symb #\/] (swap-xy (map - dir)))
-        ([char=? symb #\\] (swap-xy dir))
-        (else dir)))
+  (cond ([not symb] '())
+        ([char=? symb #\/] (list (swap-xy (map - dir))))
+        ([char=? symb #\\] (list (swap-xy dir)))
+        ([and (char=? symb #\|) (= (cadr dir) 0)] '((0 -1) (0 1)))
+        ([and (char=? symb #\-) (= (car  dir) 0)] '((-1 0) (1 0)))
+        (else (list dir))))
 
 (define (update-beam grid pos dir [symb (grid-ref grid pos)])
-  (cond ([not symb] '())
-        ([and (char=? symb #\|) (= (cadr dir) 0)]
-         (list (list (vec+ pos '(0 -1)) '(0 -1))
-               (list (vec+ pos '(0  1)) '(0  1))))
-        ([and (char=? symb #\-) (= (car dir) 0)]
-         (list (list (vec+ pos '(-1 0)) '(-1 0))
-               (list (vec+ pos '( 1 0)) '( 1 0))))
-        (else (let ([new-dir (get-new-dir symb dir)])
-                (list (list (vec+ pos new-dir) new-dir))))))
+  (map (lambda (dir) (list (map + pos dir) dir)) (get-new-dir symb dir)))
 
 (define (add-tile tiles pos dir)
   (hash-update tiles pos (lambda (t) (cons dir t)) (list dir)))
@@ -45,8 +37,23 @@
                              tiles new-beams)])
       (beams-step grid new-beams new-tiles))))
 
-(define (solve in)
+(define (solve1 in)
   (beams-step (parse in) '(((0 0) (1 0))) (hash '(0 0) (list '(1 0)))))
 
-(println (solve "input16-1.txt"))
-(println (solve "input16-2.txt"))
+(define (beam-loop-all-rows in)
+  (let* ([input (parse in)] [len (vector-length input)])
+    (map (lambda (pos)
+           (map (lambda (dir)
+                  (beams-step input (list (list pos dir)) (hash pos (list dir))))
+                '((1 0) (-1 0) (0 1) (0 -1))))
+         (append (map (lambda (x) (list x         0)) (range len))
+                 (map (lambda (x) (list x (- len 1))) (range len))
+                 (map (lambda (y) (list         0 y)) (range len))
+                 (map (lambda (y) (list (- len 1) y)) (range len))))))
+
+(define (solve2 in) (apply max (flatten (beam-loop-all-rows in))))
+
+(println (solve1 "input16-1.txt"))
+(println (solve1 "input16-2.txt"))
+(println (solve2 "input16-1.txt"))
+(println (solve2 "input16-2.txt"))
