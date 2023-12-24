96063a9dab59ffd9571643fa7a4e3052456e41fa
chrg <christian.ricci2000@gmail.com>
2023-12-24 12:34:40 +0100

    Day 23 part 2

--- 2023/day23.rkt
+++ 2023/day23.rkt
@@ -1,3 +1,5 @@
+(require racket/hash)
+
 (define (parse in)
   (list->vector (map (compose list->vector string->list) (file->lines in))))
 
@@ -30,8 +32,66 @@
              ns)))))
 
 (define (solve1 in)
-  (let* ([g (parse in)]
-         [start (start-pos g 0)]
-         [end (start-pos g (sub1 (vector-length g)))])
-    (printf "start = ~a, end = ~a\n" start end)
-    (apply max (flatten (path-count g start end (set) 0)))))
+  (let* ([g (parse in)])
+    (apply max (flatten (path-count g (start-pos g 0)
+                                    (start-pos g (sub1 (vector-length g)))
+                                    (set) 0)))))
+
+(define (get-neighbors2 g pos from)
+  (filter (lambda (p)
+            (and (not (char=? (grid-ref g p) #\#)) (not (equal? p from))))
+          (map (curry map + pos) '((1 0) (-1 0) (0 1) (0 -1)))))
+
+(define (get-paths g start end)
+  (define visited (mutable-set))
+  (define (loop pos from start i)
+    (let ([ns (get-neighbors2 g pos from)])
+      (cond ([equal? pos end] (list (list start end i)))
+            ([and (set-member? visited pos) (> (length ns) 1)]
+             (list (list start pos i)))
+            (else
+              (set-add! visited pos)
+              (if (= (length ns) 1)
+                (loop (car ns) pos start (add1 i))
+                (append* (list (list start pos i))
+                         (map (lambda (p) (loop p pos pos 1)) ns)))))))
+  (let ([next (map + start '(0 1))])
+    (loop next start start 0)))
+
+(define (make-path-graph paths)
+  (foldl (lambda (p h)
+           (hash-update h (car p)
+                        (lambda (l) (remove-duplicates (cons (cadr p) l)))
+                        (list (cadr p))))
+         (hash) (append* (map (lambda (x)
+                                (list (list (car x) (cdr x))
+                                      (list (cadr x) (list (car x) (caddr x)))))
+                              paths))))
+
+(define (graph-neighbors g n visited)
+  (filter (lambda (x) (not (set-member? visited (car x))))
+          (hash-ref g n)))
+
+(define (graph-path-count g pos end visited i)
+  (if (equal? pos end)
+    i
+    (let ([ns (graph-neighbors g pos visited)])
+      (if (empty? ns)
+        -1
+        (flatten (map (lambda (p)
+                        (graph-path-count g (car p) end
+                                          (set-add visited (car p))
+                                          (+ (cadr p) i)))
+                      ns))))))
+
+(define (solve2 in)
+  (let* ([grid (parse in)]
+         [start (start-pos grid 0)]
+         [end (start-pos grid (sub1 (vector-length grid)))]
+         [graph (make-path-graph (get-paths grid start end))])
+    (add1 (apply max (graph-path-count graph start end (set) 0)))))
+
+(println (solve1 "input23-1.txt"))
+(println (solve1 "input23-2.txt"))
+(println (solve2 "input23-1.txt"))
+(println (solve2 "input23-2.txt"))
