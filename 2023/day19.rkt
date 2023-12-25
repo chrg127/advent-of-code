39372ae98d668c06a3d67525a5196bd188794052
chrg <christian.ricci2000@gmail.com>
2023-12-25 16:38:14 +0100

    Polish day 19

--- 2023/day19.rkt
+++ 2023/day19.rkt
@@ -1,8 +1,3 @@
-(define (list-index pred lst [i 0])
-  (cond ([empty? lst] #f)
-        ([pred (car lst)] i)
-        (else (list-index pred (cdr lst) (add1 i)))))
-
 (define (cat-to-num c) (hash-ref (hash #\x 0 #\m 1 #\a 2 #\s 3) c))
 
 (define (parse-cond s)
@@ -15,9 +10,11 @@
             (cadr split)))))
 
 (define (parse-workflows line)
-  (let* ([brace-start (list-index (lambda (c) (char=? c #\{)) (string->list line))])
+  (let* ([brace-start (index-where (string->list line) (curry char=? #\{))])
     (list (substring line 0 brace-start)
-          (map parse-cond (string-split (substring line (add1 brace-start) (sub1 (string-length line))) ",")))))
+          (map parse-cond
+               (string-split (substring line (add1 brace-start)
+                                        (sub1 (string-length line))) ",")))))
 
 (define (parse-parts line)
   (map (lambda (f) (string->number (substring f 2)))
@@ -25,14 +22,16 @@
 
 (define (parse in)
   (let ([split (string-split (file->string in) "\n\n")])
-    (values (apply hash (append* (map parse-workflows (string-split (car split) "\n"))))
+    (values (apply hash (append* (map parse-workflows
+                                      (string-split (car split) "\n"))))
             (map parse-parts (string-split (cadr split) "\n")))))
 
 (define (apply-workflow part wf)
   (fourth
     (findf (lambda (rule)
              (cond ([not (list? rule)] rule)
-                   ([(second rule) (list-ref part (first rule)) (third rule)] (fourth rule))
+                   ([(second rule) (list-ref part (first rule)) (third rule)]
+                    (fourth rule))
                    (else #f)))
            wf)))
 
@@ -42,58 +41,43 @@
           ([string=? res "R"] #f)
           (else (accepted? workflows res part)))))
 
-(define (solve in)
+(define (solve1 in)
   (let-values ([(ws ps) (parse in)])
     (apply + (flatten (filter (curry accepted? ws "in") ps)))))
 
-(println (solve "input19-1.txt"))
-(println (solve "input19-2.txt"))
-
-(define (make-workflow-tree workflows name)
-  (map (lambda (rule)
-         (cond ([string=? (fourth rule) "R"] (list rule "R"))
-               ([string=? (fourth rule) "A"] (list rule "A"))
-               (else (cons rule (make-workflow-tree workflows (fourth rule))))))
-       (hash-ref workflows name)))
+(define (update-bound bounds which cat value)
+  (list-update bounds which (lambda (b) (list-set b cat value))))
 
 (define (apply-rule rule bounds)
-  (let ([inc (if (equal? (second rule) >) 1 -1)])
-    (values (list-update bounds (if (equal? (second rule) <) 1 0)
-                         (lambda (b) (list-update b (first rule)
-                                                  (lambda (value) (+ inc (third rule))))))
-            (list-update bounds (if (equal? (second rule) <) 0 1)
-                         (lambda (b) (list-update b (first rule)
-                                                  (lambda (value) (third rule))))))))
+  (let ([inc (if (equal? (second rule) >) 1 -1)] [val (third rule)])
+    (if (= val 0) ; special case
+      (values bounds bounds)
+      (values (update-bound bounds (if (= inc 1) 0 1) (first rule) (+ inc val))
+              (update-bound bounds (if (= inc 1) 1 0) (first rule) val)))))
 
-(define (walk-tree t bounds [i 0])
-  ; (printf "~abounds = ~a\n" (make-string (* i 4) #\ ) bounds)
-  (if (string? (car t))
-    (begin ;(printf "~afinal bounds = ~a\n" (make-string (* i 4) #\ ) bounds)
-           (if (string=? (car t) "R") '() (list bounds)))
-    (append*
-      (filter (lambda (x) (not (empty? x)))
-              (cadr
-                (foldl (lambda (c r)
-                         (let ([cur-bounds (car r)]
-                               [bounds-list (cadr r)])
-                           (let-values ([(bound-if-true bound-if-false)
-                                         (apply-rule (car c) cur-bounds)])
-                             ; (printf "~achecking ~a\n" (make-string (* i 4) #\ ) (fourth (car c)))
-                             (list bound-if-false
-                                   (cons (walk-tree (cdr c) bound-if-true (+ i 1))
-                                         bounds-list)))))
-                       (list bounds '()) t))))))
+(define (walk-rules workflows name bounds)
+  (foldl (lambda (rule r)
+           (let ([cur-bounds (car r)] [collected (cadr r)])
+             (let-values ([(if-true if-false) (apply-rule rule cur-bounds)])
+               (list if-false (cons (walk-tree workflows (fourth rule) if-true)
+                                    collected)))))
+         (list bounds '()) (hash-ref workflows name)))
 
-(define (mul-bounds bs)
-  (apply * (map (lambda (x y) (add1 (- x y)))
-                (cadr bs) (car bs))))
+(define (walk-tree workflows name bounds)
+  (cond ([equal? name "R"] '())
+        ([equal? name "A"] (list bounds))
+        (else (append*
+                (filter (lambda (x) (not (empty? x)))
+                        (cadr (walk-rules workflows name bounds)))))))
+
+(define (mul-bounds bs) (apply * (map (compose add1 -) (cadr bs) (car bs))))
 
 (define (solve2 in)
-  (let*-values ([(workflows parts) (parse in)]
-                [(tree) (make-workflow-tree workflows "in")]
-                [(bounds) (walk-tree tree '((1 1 1 1) (4000 4000 4000 4000)))]
-                [(result) (apply + (remove-duplicates (map mul-bounds bounds)))])
-    result))
+  (define start-bounds '((1 1 1 1) (4000 4000 4000 4000)))
+  (let-values ([(workflows parts) (parse in)])
+    (apply + (map mul-bounds (walk-tree workflows "in" start-bounds)))))
 
+(println (solve1 "input19-1.txt"))
+(println (solve1 "input19-2.txt"))
 (println (solve2 "input19-1.txt"))
 (println (solve2 "input19-2.txt"))
